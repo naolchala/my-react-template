cbc27323e772187ef9256ad22405e2566b709cb0
naolchala <naolchala6@gmail.com>
2021-02-02 19:05:36 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.scss";
+
+export const App = () => {
+    return (
+        <div className="App">
+            <h1>Hello React</h1>
+        </div>
+    );
+};
